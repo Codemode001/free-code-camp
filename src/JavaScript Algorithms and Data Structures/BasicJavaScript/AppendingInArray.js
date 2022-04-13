cf4ec7ef72b5f9988855b0eb2b3595338f6e9abe
Codemode001 <haroldrex12331@gmail.com>
2022-04-13 16:45:51 +0800

    update

---
+++ src/JavaScript Algorithms and Data Structures/BasicJavaScript/AppendingInArray.js
@@ -0,0 +1,8 @@
+// Setup
+const myArray = [
+  ["John", 23],
+  ["cat", 2],
+];
+myArray.push(["dog", 3]);
+// Only change code below this line
+//Append means add something at the end.
---
+++ src/JavaScript Algorithms and Data Structures/BasicJavaScript/manipulateArrays.js
@@ -0,0 +1,8 @@
+const myArray = [
+  ["John", 23],
+  ["cat", 2],
+];
+const removedFromMyArray = myArray.pop(); //removed the last array and assigned the removed array to removedFromMyArray
+
+console.log(removedFromMyArray); //printed the removed array
+console.log(myArray); //printed the remaining array
