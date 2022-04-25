e838ac6e479704815401b261403c017cf211d173
Codemode001 <haroldrex12331@gmail.com>
2022-04-26 00:23:03 +0800

    code

---
+++ src/Frontend Development Libraries/React/code2.jsx
@@ -0,0 +1,13 @@
+const JSX = (
+  <div>
+    <h1>dffd</h1>
+
+    <p>sdfsdf</p>
+
+    <ul>
+      <li>dfssd</li>
+      <li>dsfsd</li>
+      <li>sdfs</li>
+    </ul>
+  </div>
+);
---
+++ src/Frontend Development Libraries/React/code3.jsx
@@ -0,0 +1,7 @@
+const JSX = (
+  <div>
+    <h1>This is a block of JSX</h1>
+    {/*<h2>hello world!</h2>*/}
+    <p>Here's a subtitle</p>
+  </div>
+);
