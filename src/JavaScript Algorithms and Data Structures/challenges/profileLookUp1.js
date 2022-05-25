717c4bc0591b57aee176a6a581a7f033fc56b15c
Codemode001 <haroldrex12331@gmail.com>
2022-05-25 14:46:40 +0800

    Update profileLookUp1.js

--- src/JavaScript Algorithms and Data Structures/challenges/profileLookUp1.js
+++ src/JavaScript Algorithms and Data Structures/challenges/profileLookUp1.js
@@ -29,6 +29,7 @@ const contacts = [
 function lookUpProfile(name, prop) {
   // Only change code below this line
   for (let i = 0; i < contacts.length; i++) {
+    //by doing this si na karon is naay value nga 0 1 2 3 which is mura na syag nahimog id per objects nga naa sa contacts
     if (contacts[i].firstName === name) {
       if (contacts[i].hasOwnProperty(prop)) {
         return contacts[i][prop]; //namali ko ani more than 2 hours ko na nag code ani akong gibutang kay return contacts[i].prop which is mali kay wala man tay key nga prop sa atoang objects. dapat atong gitawag is si [prop] nga parameter sa atong function unya naay property nga likes sa ubos
